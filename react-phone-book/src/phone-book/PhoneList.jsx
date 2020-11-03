85f16e01cf07e477ea66585cc84c9b5f565ee709
smskit726 <smskit726@gmail.com>
2020-11-03 13:41:15 +0900

    React Phone_Book

--- react-phone-book/src/phone-book/PhoneList.jsx
+++ react-phone-book/src/phone-book/PhoneList.jsx
@@ -5,6 +5,7 @@ const PhoneList = ({ phoneBook }) => {
   const phoneItems = phoneBook.map((phone) => {
     return (
       <tr>
+        <td>{phone.id}</td>
         <td>{phone.name}</td>
         <td>{phone.number}</td>
       </tr>
@@ -13,6 +14,7 @@ const PhoneList = ({ phoneBook }) => {
   return (
     <table>
       <tr>
+        <th>ID</th>
         <th>이름</th>
         <th>전화번호</th>
       </tr>
