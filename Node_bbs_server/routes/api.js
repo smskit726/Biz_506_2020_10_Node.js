74980e4ff42d33489710d3f2d1a98f3dc5409d31
smskit726 <smskit726@gmail.com>
2020-11-13 16:46:46 +0900

    nodejs-MySQL

--- Node_bbs_server/routes/api.js
+++ Node_bbs_server/routes/api.js
@@ -3,6 +3,7 @@
 // 현재 nodejs가 지원하는 문법
 const express = require("express");
 const router = express.Router();
+const { bbsDao } = require("../models/index");
 
 router.get("/", (req, res) => {
   //   res.send("반갑습니다.");
@@ -15,7 +16,27 @@ router.get("/bbsList", (req, res) => {
     { id: 1, writer: "이몽룡", subject: "게시판" },
     { id: 2, writer: "성춘향", subject: "게시판" },
   ];
-  res.json(list);
+  bbsDao.findAll({ order: [{ b_date_time: "DESC" }] }).then((bbsList) => {
+    res.json(bbsList);
+  });
+  //   res.json(list);
+});
+
+router.get("/insert", (req, res) => {
+  bbsDao
+    .create({
+      b_writer: req.query.writer || "이몽룡",
+      b_date_time: Date().toString(),
+      b_subject: "게시판 작성",
+      b_content: "DB연결 어렵다 :D",
+    })
+    .then((result) => {
+      //   res.json(result);
+      res.redirect("/api/bbsList");
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
